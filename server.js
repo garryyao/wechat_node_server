58cdbea2f4d23dfc9414ada48f1c524e9372c410
John Heroy <johnheroy@gmail.com>
2014-06-16 11:25:44 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require("http");
+var Firebase = require('firebase');
+var myRootRef = new Firebase('https://ef-play-demo.firebaseio.com/');
+
+http.createServer(function(request, response) {
+	// code
+}).listen(8080);
