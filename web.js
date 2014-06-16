326f2b11f1f088265f2224c7389290db0610fa1a
John Heroy <johnheroy@gmail.com>
2014-06-16 18:02:38 +0800

    implement wechat verification

--- web.js
+++ web.js
@@ -1,9 +1,26 @@
-// web.js
 var express = require("express");
+var sha1 = require('sha1');
 var app = express();
 
 app.get('/', function(req, res) {
   res.send('Hello, EF!');
+
+  // wechat verification
+  signature = req.params.signature;
+  timestamp = req.params.timestamp;
+  nonce = req.params.nonce;
+  echostr = req.params.echostr;
+  TOKEN = "efef";
+
+  tmpArr = [TOKEN, timestamp, nonce];
+  tmpStr = sha1(tmpArr.sort().join());
+
+  if (signature === tmpStr) {
+  	res.writeHead(200);
+  	res.write(echostr);
+  	res.end();
+  }
+
 });
 
 var port = Number(process.env.PORT || 5000);
