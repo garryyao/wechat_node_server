edca2837ecbc3aee3ff71f346df7f4cdb5271ef2
John Heroy <johnheroy@gmail.com>
2014-06-16 11:39:03 +0800

    renamed file

--- web.js
+++ web.js
@@ -3,5 +3,7 @@ var Firebase = require('firebase');
 var myRootRef = new Firebase('https://ef-play-demo.firebaseio.com/');
 
 http.createServer(function(request, response) {
-	// code
-}).listen(8080);
+	response.writeHead(200);
+	response.write("Hello, EF!");
+	response.end();
+}).listen(3000);
