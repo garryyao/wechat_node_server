2912994f0bdc0304b05f321c057f6a88eb899eae
John Heroy <johnheroy@gmail.com>
2014-06-16 16:59:27 +0800

    Change msg

--- web.js
+++ web.js
@@ -1,12 +1,9 @@
 // web.js
 var express = require("express");
-var logfmt = require("logfmt");
 var app = express();
 
-app.use(logfmt.requestLogger());
-
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello, EF!');
 });
 
 var port = Number(process.env.PORT || 5000);
