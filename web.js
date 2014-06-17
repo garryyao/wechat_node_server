e5b5571ec0ea7bf604ea70f3a01c6d4a7cf6ff01
John Heroy <johnheroy@gmail.com>
2014-06-17 11:04:39 +0800

    created basic echo response

--- web.js
+++ web.js
@@ -16,6 +16,26 @@ app.get('/', function(req, res) {
     }
 });
 
+weixin.textMsg(function(msg) {
+    console.log("textMsg received");
+    console.log(JSON.stringify(msg));
+
+    var resMsg = {};
+
+    switch (msg.content) {
+        case "文本" :
+            // 返回文本消息
+            resMsg = {
+                fromUserName : msg.toUserName,
+                toUserName : msg.fromUserName,
+                msgType : "text",
+                content : "这是文本回复",
+                funcFlag : 0
+            };
+            break;
+    }
+}
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
