1a796c64e5a494d07a9b7472555064fc70a18d6f
John Heroy <johnheroy@gmail.com>
2014-06-17 15:57:31 +0800

    add new case switch

--- web.js
+++ web.js
@@ -1,21 +1,26 @@
-// declarations
+// app declarations
 var express = require("express");
 var weixin = require("weixin-api");
 var connect = require("connect");
-var Firebase = require('firebase');
-var myRootRef = new Firebase('https://ef-play-demo.firebaseIO.com/');
 var app = express();
-
 app.use(connect.json());
 app.use(connect.urlencoded());
 app.use(connect.multipart());
 
-// config
+// wechat config
 weixin.token = "efef";
+var APP_ID = "wx4ee5ca70e09083cb";
+var APP_SECRET = "c6c24defe7cd98877a81a1b0cf67969d";
 
-// access verification
+// firebase declarations
+var Firebase = require('firebase');
+var firebase_url = 'https://ef-play-demo.firebaseio.com/';
+var fireRoute = new Firebase(firebase_url);
+var messages = fireRoute.child('messages');
+var users = fireRoute.child('users');
+
+// authenticate signature
 app.get('/', function(req, res) {
-    // check signature
     if (weixin.checkSignature(req)) {
         res.send(200, req.query.echostr);
     } else {
@@ -23,11 +28,17 @@ app.get('/', function(req, res) {
     }
 });
 
+// receive text message event
 weixin.textMsg(function(msg) {
     console.log("textMsg received");
     console.log(JSON.stringify(msg));
 
+    
+
     var resMsg = {};
+
+
+
 
     switch (msg.content) {
         case "hello" :
@@ -49,14 +60,26 @@ weixin.textMsg(function(msg) {
         		funcFlag : 0
         	};
         	break;
+
+        case "get app id" :
+        	resMsg = {
+        		fromUserName : msg.toUserName,
+        		toUserName : msg.fromUserName,
+        		msgType : "text",
+        		content : APP_ID,
+        		funcFlag : 0
+        	};
+        	break;
     }
 
     weixin.sendMsg(resMsg);
 });
 
+// listen for new messages and send to wechat users
+
+
 // Start
 app.post('/', function(req, res) {
-    // loop
     weixin.loop(req, res);
 });
 
