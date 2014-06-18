64efbdc0e9f629c67ffd9b7eb8797e245b7fa35e
John Heroy <johnheroy@gmail.com>
2014-06-18 16:33:27 +0800

    implemented broken access_token

--- web.js
+++ web.js
@@ -11,8 +11,9 @@ app.use(connect.urlencoded());
 // wechat config
 weixin.token = "efef";
 var WEIXIN_HAO = "gh_a5956fdd03e2";
-var APP_ID = "wx4ee5ca70e09083cb";
-var APP_SECRET = "c6c24defe7cd98877a81a1b0cf67969d";
+var APP_ID = "wx3bd6462d57a03618";
+var APP_SECRET = "f59d49264e430799579bd831a1927f0b";
+var ACCESS_TOKEN;
 
 // firebase declarations
 var Firebase = require('firebase');
@@ -36,6 +37,7 @@ weixin.textMsg(function(msg) {
     console.log("textMsg received");
     console.log(JSON.stringify(msg));
 
+    // add message to firebase
     var name = "John";
     var text = msg.content;
     messages.push({ name: name, text: text });
@@ -82,65 +84,72 @@ weixin.textMsg(function(msg) {
 messages.on('child_added', function(snapshot) {
 	var message = snapshot.val();
 	var formatted_message = message.name + " says: " + message.text;
-	
 	console.log(formatted_message);
 
-	/*
-	var pushMsg = {};
-	pushMsg = {
-		fromUserName : WEIXIN_HAO,
-		toUserName : "owHEYt8FZJVTvs3rp_3ra9tc-wfI",
-		msgType : "text",
-		content : formatted_message,
-		funcFlag : 0
-	};
-	console.log(pushMsg);
-	*/
-
 	// fetch access_token in order to 发送客服消息
-	var accessTokenURL = "https://api.wechat.com/cgi-bin/token?grant_type=client_credential&appid="+APP_ID+"&secret="+APP_SECRET;
-	var accessTokenOptions = {
-		method: "GET",
-		url: accessTokenURL,
-	};
+	
 
-	function accessTokenCallback (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			var access_token = JSON.parse(body).access_token;
-			console.log(access_token);
+	// if the token is valid
+	// then use the token and go straight to pushing the chat
+	// else
+	// fetch the token now and then push the chat after (in a callback)
 
-			// now push the chat to the user
-			var pushChatURL = "https://api.wechat.com/cgi-bin/message/custom/send?access_token="+access_token;
-			var pushChatOptions = {
-				method: "POST",
-				url: pushChatURL,
-				body: JSON.stringify({
-					"touser" : "owHEYt8FZJVTvs3rp_3ra9tc-wfI",
-					"msgtype" : "text",
-					"text" :
-					{
-						"content" : formatted_message
-					}
-				})
-			};
-
-			function pushChatCallback (error, response, body) {
-				if (!error && response.statusCode == 200) {
-					console.log("The message " + formatted_message + " was successfully delivered.");
-					console.log(body);
-				}
-			}
-
-			request(pushChatOptions, pushChatCallback);
-		}
+	if (!!ACCESS_TOKEN && ((new Date().getTime()) < ACCESS_TOKEN.expiration)) {
+		console.log("Pushing message without getting new token");
+		pushChat();
+	} else {
+		console.log("Pushing message and getting a new token");
+		getToken();
 	}
 
-	request(accessTokenOptions, accessTokenCallback);
+	function pushChat() {
+		var pushChatURL = "https://api.wechat.com/cgi-bin/message/custom/send?access_token="+ACCESS_TOKEN.access_token;
+		var pushChatOptions = {
+			method: "POST",
+			url: pushChatURL,
+			body: JSON.stringify({
+				"touser" : "owHEYt8FZJVTvs3rp_3ra9tc-wfI",
+				"msgtype" : "text",
+				"text" :
+				{
+					"content" : formatted_message
+				}
+			})
+		};
 
+		function pushChatCallback (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				console.log(body);
+				// if the message comes back ok i.e. "errmsg" is "ok", then change status to read for wechat users
+			}
+		}
 
+		request(pushChatOptions, pushChatCallback);
+	}
+
+	function getToken() {
+		var accessTokenURL = "https://api.wechat.com/cgi-bin/token?grant_type=client_credential&appid="+APP_ID+"&secret="+APP_SECRET;
+		var accessTokenOptions = {
+			method: "GET",
+			url: accessTokenURL,
+		};
+
+		function accessTokenCallback (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var data = JSON.parse(body);
+				ACCESS_TOKEN = new Object();
+				ACCESS_TOKEN.access_token = data.access_token;
+				ACCESS_TOKEN.expiration = (new Date().getTime()) + (data.expires_in - 10) * 1000;
+
+				console.log(ACCESS_TOKEN);
+
+				// now push the chat to the user
+				pushChat();
+			}
+		}
+		request(accessTokenOptions, accessTokenCallback);
+	}
 });
-
-
 
 
 // Start
